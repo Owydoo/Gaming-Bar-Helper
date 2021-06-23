c13b15d380fa8eb0c6e69f591bf3bef0e3a41d19
Hugo <owydoo@gmail.com>
2021-06-23 17:37:58 +0200

    Added reset button and pause button to timers

--- src/app/pccard/pccard.component.ts
+++ src/app/pccard/pccard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pccard',
@@ -6,6 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pccard.component.scss']
 })
 export class PCCardComponent implements OnInit {
+
+  @Input()
+  get name(): string { return this._name; }
+  set name(name: string) {
+    this._name = (name && name.trim()) || '<no name set>';
+  }
+  private _name = 'Default Name';
+
+  @Input()
+  get number(): number { return this._number;}
+  set number(nb:number){
+    this._number = nb;
+  }
+  private _number = 0;
+
+
 
   constructor() { }
 
--- src/app/pccard/pctimer/pctimer.component.ts
+++ src/app/pccard/pctimer/pctimer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pctimer',
@@ -6,68 +6,95 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pctimer.component.scss'],
 })
 export class PctimerComponent implements OnInit {
-  public hour:  number = 0;
+  public hour: number = 0;
   public minute: number = 0;
   public second: number = 0;
 
   public time: string = '00:00:00';
   public nbSecond: number = 0;
 
+  public timerIsStartedOnce: boolean = false;
   public timerIsOn: boolean = false;
+
+  public timerInterval: any;
 
   constructor() {}
 
-  resetTimer() {
-    console.log('reset timer');
-  }
 
   ngOnInit(): void {}
 
   launchTimer() {
-    if (!this.timerIsOn) {
-      this.timerIsOn = true;
-      setInterval(() => {
-        this.update();
-      }, 1000);
+    if (this.timerIsOn) {
+      this.timerIsOn = false
     }
+    else {
+      this.timerIsOn = true
+    }
+    
+    this.timerIsStartedOnce = true;
+    this.timerInterval = setInterval(() => {
+      this.update();
+    }, 1000);
+  
+    // else {
+    //   if (this.timerIsOn) {
+    //     this.pauseTimer()
+    //   }
+    // }
+  }
+
+  pauseTimer() {
+    this.timerIsOn = false;
+    clearInterval(this.timerInterval)
+  }
+
+  resetTimer(){
+    this.timerIsOn = false;
+    clearInterval(this.timerInterval)
+
+    this.hour = 0;
+    this.minute = 0;
+    this.second = 0;
+    this.time = '00:00:00';
+    this.nbSecond = 0;
+
+
   }
 
   update() {
     this.handleTimer();
-    
+    // if (this.timerIsOn) {
+    //   clearInterval(this.timerInterval)
+    // }
   }
 
-  handleTimer(){
+  handleTimer() {
     this.nbSecond += 1;
-    this.second = (this.nbSecond % 60);
-    this.minute = (Math.floor(this.nbSecond/60) % 60);
-    this.hour = (Math.floor(this.nbSecond/3600)  % 3600);
+    this.second = this.nbSecond % 60;
+    this.minute = Math.floor(this.nbSecond / 60) % 60;
+    this.hour = Math.floor(this.nbSecond / 3600) % 3600;
 
-    var hStr = ""
+    var hStr = '';
     if (this.hour < 10) {
       hStr = '0' + this.hour.toString();
-    }
-    else {
+    } else {
       hStr = this.hour.toString();
     }
 
-    var mStr = ""
+    var mStr = '';
     if (this.minute < 10) {
       mStr = '0' + this.minute.toString();
-    }
-    else {
+    } else {
       mStr = this.minute.toString();
     }
 
-    var sStr = ""
+    var sStr = '';
     if (this.second < 10) {
       sStr = '0' + this.second.toString();
-    }
-    else {
+    } else {
       sStr = this.second.toString();
     }
 
-    this.time = hStr + ":" + mStr + ":" + sStr;
-    
+    this.time = hStr + ':' + mStr + ':' + sStr;
   }
 }
